dee098afb650f77a15f252bf7062ed80522b9eb6
lancejake-9 <lance.jake13@gmail.com>
2025-08-14 11:43:28 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+/* Simple offline-first cache */
+const VERSION = '1.9.0';
+const CACHE = 'ff-ddm-cache-' + VERSION;
+const CORE = [
+  './',
+  './index.html',
+  './offline.html',
+  './manifest.webmanifest',
+  './icons/apple-touch-icon-180.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('./offline.html')))
+  );
+});
